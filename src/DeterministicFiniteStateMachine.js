5b5aa2cbe54edec2ffcd4d31eec2ade01eaebe83
bryanmonca <49891989+bryanmonca@users.noreply.github.com>
2020-02-28 13:18:32 -0800

    Add files via upload

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -1,6 +1,5 @@
-/* We can do it recursively, blindly, different implementations */
-
 export default class DeterministicFiniteStateMachine {
+
   /**
    */
   constructor({ transitions, startState, acceptStates }) {
@@ -9,21 +8,20 @@ export default class DeterministicFiniteStateMachine {
     this.acceptStates = acceptStates;
   }
 
-  /**
-   *  Tells me what the alphabet is, by a given machine
-   */
   alphabet() {
-      const alphabet = new Set(); // mapping or load-dash
-
-      for(const [key, desc] of Object.entries(this.transitions)) {
-          for (const symbol of Object.keys(desc)) {
-              alphabet.add(symbol);
-          }
+    const alphabet = new Set();
+    
+    for(const [state, desc] of Object.entries(this.transitions)) {
+      for(const symbol of Object.keys(desc)) {
+        alphabet.add(symbol);
       }
+    }
+
+    return alphabet.values();
   }
 
   states() {
-    return Object.keys(this.transitions);
+    return new Set(Object.keys(this.transitions));
   }
 
   stateAccepted(state) {
@@ -35,6 +33,7 @@ export default class DeterministicFiniteStateMachine {
    * @returns a string state name
    */
   transition(state, symbol) {
+    if(!this.transitions[state]) return;
     return this.transitions[state][symbol];
   }
 
@@ -50,56 +49,59 @@ export default class DeterministicFiniteStateMachine {
  *
  */
 export function cross(dfa1, dfa2, accepts = (dfa1State, dfa2State) => true) {
-//.... TODO
   const acceptStates = [];
   const transitions = {};
-  
   const alphabet = new Set([...dfa1.alphabet(), ...dfa2.alphabet()]);
 
-  const stateName = (state1, state2) => `m1:${state1}xm2:${state2}`; //name of new state
+  // A function which returns a state name for a state in machine 1 and a state in machine 2 
+  const stateName = (state1, state2) => `m1:${state1}xm2:${state2}`;
 
   const startState = stateName(dfa1.startState, dfa2.startState);
+  const unresolvedStates = [{ state: startState, state1: dfa1.startState, state2: dfa2.startState }];
 
-  //list of unresolved states
-  const unresolvedStates = [{ state: startState, state1: dfa1.startState, state2: dfa2.startState}];
+  while(unresolvedStates.length > 0) {
+    const { state1, state2, state } = unresolvedStates.pop();
 
-  while(unresolvedStates.length > 0) {  
-      const { state, state1, state2} = unresolvedStates.pop();
+    transitions[state] = {};
+    if(accepts(state1, state2)) acceptStates.push(state);
 
-      transitions[state] = {};
+    for(const symbol of alphabet) {
+      const nextState1 = dfa1.transition(state1, symbol);
+      const nextState2 = dfa2.transition(state2, symbol);
 
-      if(accepts(state1, state2)) acceptStates.push(state);
+      const nextState = stateName(nextState1, nextState2);
+      transitions[state][symbol] = nextState;
 
-      //what state for a given transition
-      for(const symbol of alphabet) {
-          const nextState1 = dfa1.transition(state1, symbol);
-          const nextState2 = dfa2.transition(state2, symbol);
-
-          const nextState = stateName(nextState1, nextState2);
-          transitions[state][symbol] = nextState;
-
-          if(!transitions[nextState]) {
-              unresolvedStates.push({ state: nextState, state1: nextState1, state2: nextState2});
-          }
+      if(!transitions[nextState]) {
+        // recording that we need to process this state
+        unresolvedStates.push({ state: nextState, state1: nextState1, state2: nextState2 });
       }
-      
-  }
+    }
+  } 
 
-
-  return new DeterministicFiniteStateMachine({ transitions, startState, acceptStates});
+  return new DeterministicFiniteStateMachine({
+    acceptStates,
+    startState,
+    transitions
+  });
 }
 
 export function union(dfa1, dfa2) {
   return cross(dfa1, dfa2, 
-   (state1, state2) => dfa1.stateAccepted(state1) || dfa2.stateAccepted(state2));
+    (state1, state2) => dfa1.stateAccepted(state1) || dfa2.stateAccepted(state2));
 }
 
 export function intersection(dfa1, dfa2) {
   return cross(dfa1, dfa2, 
-   (state1, state2) => dfa1.stateAccepted(state1) && dfa2.stateAccepted(state2));
+    (state1, state2) => dfa1.stateAccepted(state1) && dfa2.stateAccepted(state2));
 }
 
 export function minus(dfa1, dfa2) {
   return cross(dfa1, dfa2, 
-   (state1, state2) => dfa1.stateAccepted(state1) && !dfa2.stateAccepted(state2));
+    (state1, state2) => dfa1.stateAccepted(state1) && !dfa2.stateAccepted(state2));
+}
+
+export function minimize(dfa) {
+  // TODO return a new minimized DFA
+  return dfa;
 }
